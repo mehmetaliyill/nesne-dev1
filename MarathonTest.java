9fdcc282101fc3c15d031f1c279ff4429b90914d
mehmetaliyill <146987558+mehmetaliyill@users.noreply.github.com>
2024-03-04 22:33:45 +0300

    Add files via upload

---
+++ MarathonTest.java
@@ -0,0 +1,27 @@
+public class MarathonTest {
+    public static void main(String[] args) {
+        Marathon marathon = new Marathon();
+        marathon.addRacer(341, "Kadir");
+        marathon.addRacer(273, "Gökhan");
+        marathon.addRacer(278, "Hakan");
+        marathon.addRacer(329, "Suzan");
+        marathon.addRacer(445, "Pınar");
+        marathon.addRacer(402, "Mehmet");
+        marathon.addRacer(388, "Ali");
+        marathon.addRacer(270, "Emel");
+        marathon.addRacer(243, "Fırat");
+        marathon.addRacer(334, "James");
+        marathon.addRacer(412, "Jale");
+        marathon.addRacer(393, "Ersin");
+        marathon.addRacer(299, "Deniz");
+        marathon.addRacer(343, "Gözde");
+        marathon.addRacer(317, "Anıl");
+        marathon.addRacer(265, "Burak");
+
+
+
+        marathon.ilkUc();
+        
+        marathon.abc();
+    }
+}
